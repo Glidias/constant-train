4c5778646d2e64145a5ad5766875492850b0915c
Glidias <glidias@hotmail.com>
2017-03-02 19:38:08 +0800

    wip accelerate+deaccelerate calc

--- src/cstrain/h2d/TrainBGScene.hx
+++ src/cstrain/h2d/TrainBGScene.hx
@@ -8,6 +8,8 @@ import h2d.Tile;
 import h2d.css.Fill;
 import h3d.mat.Data.TextureFlags;
 import h3d.mat.Texture;
+import hxd.Key;
+import hxd.Timer;
 
 import h2d.css.Defs;
 
@@ -27,6 +29,7 @@ class TrainBGScene extends AbstractBGScene implements IBGTrain
 	public function new() 
 	{
 		super();
+		calcPickupTime();
 	}
 	
 
@@ -35,14 +38,43 @@ class TrainBGScene extends AbstractBGScene implements IBGTrain
 
 	var _curLoc:Float = 0;
 	
-	var _targetDest:Float;
-	var _maxSpeed:Float = 1;
+	var _targetDest:Float = 0;
+	
 	var _reseting:Bool = true;
 	
 	var _cruisingSpeed:Float = 1;  
 	static inline var PUSH_FORWARD_ERROR:Float = .75;
 	
+	
+	var _maxSpeed:Float = 1;
+	var _isStarted:Bool = false;
+	var _startIndex:Int = -1;
+	var _tweenProgress:Float = 0;
+	var _tweenDuration:Float = 0;
+
+	
+	// cubic deriative engine
+	var _pickupTime:Float;
+	var _pickupTimeDiff:Float; // difference in time with pickup acceleration speed compared to constant speed
+	var _pickupTimeDistCovered:Float;
+	function calcPickupTime():Void {	
+		//    find x _pickupTime  where   ( dy/dx(f(x) == maxSpeed  )   where f(x) is a cubic function of x^3
+		_pickupTime = Math.sqrt(1 / 3 * _maxSpeed);
+		
+
+		// precalculate difference in time compared to linear function of (maxSpeed*x).  (ie. differenceInTime = pickupTime - timeItTakesToCoverPickupDistWithLinearFunction)
+		//  y = pickupTime*pickupTime*pickupTime;   // distance covered by cubic acceleration of speed
+		_pickupTimeDistCovered = _pickupTime * _pickupTime * _pickupTime;
+		//  Sub y distance into: y =  _maxSpeed * pickupTime;  and find x  // ie. timeItTakesToCoverPickupDistWithLinearFunction
+		//  pickupTime*pickupTime*pickupTime = _maxSpeed * x
+		//  x = pickupTime*pickupTime*pickupTime/ maxSpeed;
+		// So, compare difference in x as below..
+		_pickupTimeDiff = _pickupTime -  _pickupTimeDistCovered / _maxSpeed;
+		//trace(_pickupTime + ", " + _pickupTimeDiff);
+	}
 	/* INTERFACE cstrain.core.IBGTrain */
+	
+	var unitTimeLength:Float = hxd.Timer.wantedFPS  / 1;		// total frames for each unitTimeLength
 	
 	public function resetTo(index:Int):Void 
 	{
@@ -51,9 +83,25 @@ class TrainBGScene extends AbstractBGScene implements IBGTrain
 		
 	}
 	
+	var _startIndexTime:Float;
+	
 	public function travelTo(index:Int):Void 
 	{
 		_targetDest = index;
+		
+		if (!_isStarted) {
+			_isStarted = true;
+			_startIndex = Std.int(_curLoc);
+			_tweenProgress = 0;
+			_tweenDuration = (index - _startIndex ) * unitTimeLength/_maxSpeed +  _pickupTimeDiff*2 * unitTimeLength;  // consider time difference of both acceleration and de-acceleration 
+			_startIndexTime = haxe.Timer.stamp();
+			
+			
+		}
+		else {
+			_tweenDuration = (index - _startIndex) * unitTimeLength/_maxSpeed  + _pickupTimeDiff*2*unitTimeLength;
+		}
+		
 	}
 	
 	public function stopAt(index:Int):Void 
@@ -73,6 +121,7 @@ class TrainBGScene extends AbstractBGScene implements IBGTrain
 	public function setMaxSpeed(maxSpeed:Float):Void 
 	{
 		_maxSpeed = maxSpeed;
+		calcPickupTime();
 	}
 	
 	public function setCruisingSpeed(speed:Float):Void 
@@ -82,13 +131,96 @@ class TrainBGScene extends AbstractBGScene implements IBGTrain
 	
 	// ---
 	
-	
+
 	// Called each frame
+	
+	var curTime:Float = 0;
+	
+	
 	override function update(dt:Float) {
-		for ( entity in entities)
-		{
-			entity.update(dt*1);
+	
+	
+		if (Key.isPressed(Key.G)) {
+			travelTo( Std.int(_targetDest + 2) );
+		
 		}
+		
+		if (!Key.isPressed(Key.H)) {
+		//return;
+		
+		}
+	
+		//else return;
+		
+		
+		if (_isStarted) {
+	
+			
+			if (_tweenProgress > _tweenDuration) _tweenProgress = _tweenDuration;  // force clammp tween progress to duration
+			
+			var pickupTimeDur:Float = _pickupTime * unitTimeLength;
+			
+			var tarLoc:Float = 0;
+			if (pickupTimeDur * 2 >= _tweenDuration) {
+				trace("TODO: half pickup/pickdown case!!");
+				tarLoc = CSMath.lerp( _startIndex, _targetDest,  _tweenProgress / _tweenDuration);
+			}
+			else if (_tweenProgress < pickupTimeDur) {
+				trace("PICKUP:" + _curLoc);
+				//_curLoc = (_tweenProgress / pickupTimeDur);
+				//_curLoc = _curLoc * _curLoc * _curLoc;
+				tarLoc = (_tweenProgress / unitTimeLength);	
+				tarLoc = tarLoc * tarLoc * tarLoc;
+				
+			}
+			else if ( _tweenProgress >=  _tweenDuration  - pickupTimeDur) {
+				trace("PICKDOWN:" + _curLoc + "/" + _targetDest);
+			//	tarLoc = _tweenProgress - _tweenDuration - pickupTimeDur;
+			//	tarLoc = _tweenProgress / unitTimeLength;
+				//tarLoc = ((_tweenDuration-_tweenProgress) / unitTimeLength);	
+				//trace((_tweenDuration-_tweenProgress));
+				
+				tarLoc = _tweenProgress - _tweenDuration + pickupTimeDur;
+				tarLoc /= unitTimeLength;
+		
+			
+				tarLoc = tarLoc * tarLoc * tarLoc;
+				
+				tarLoc += _targetDest - _pickupTime;
+				//_isStarted = false;
+						
+			}
+			else {
+				tarLoc = CSMath.lerp( _pickupTimeDistCovered, _targetDest - _pickupTime, (_tweenProgress - pickupTimeDur) / (_tweenDuration -pickupTimeDur*2 ) );
+		
+			}
+			
+			var diff = (tarLoc - _curLoc)*unitTimeLength;
+			if (tarLoc >= _targetDest) {
+				tarLoc = _targetDest;
+				diff = (tarLoc - _curLoc) * unitTimeLength;
+			}
+		//	trace( _tweenProgress / _tweenDuration);
+			for ( entity in entities)
+			{
+				entity.update(diff);
+			}
+			
+			_curLoc = tarLoc;
+			
+			if (tarLoc >= _targetDest) {
+				_isStarted = false;
+				trace("END:" + tarLoc);
+				
+			}
+			_tweenProgress += dt;
+			
+		}
+		
+	
+		
+		
+		
 		
 		//emitter.step();
 		
