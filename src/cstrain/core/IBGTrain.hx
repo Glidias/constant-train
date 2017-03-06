01a1451b85fadc6fb57816418f454038697edf63
Glidias <glidias@hotmail.com>
2017-03-06 17:30:16 +0800

    client server comment

--- src/cstrain/core/IBGTrain.hx
+++ src/cstrain/core/IBGTrain.hx
@@ -5,7 +5,9 @@ package cstrain.core;
  */
 interface IBGTrain 
 {
-	// Send (NOTE: for multiplayer support, this should be factored out to an IBGTrainServer sub-implementation, actually..)
+	// SERVER/CLIENT
+	
+	// Send 
 	function resetTo(index:Int):Void;
 	function travelTo(index:Int):Void;
 	function stopAt(index:Int):Void; 
@@ -14,17 +16,18 @@ interface IBGTrain
 	// maximum amount of indices the train can move in a single second. Often used in gameplay.
 	function setMaxSpeed(maxSpeed:Float):Void;
 	
-	
 	// Server will likely just return any changes to critical values such as: _startIndex/_tweenProgress/_tweenDuration/targetDest/movingState, and clients will reflect changes accordingly in their own update loops.
 	// Thus, server need not run any per frame executions per client.
 	
 	// --------------------------
-	
-	// cruising speed => "comfortable" moving speed. Currently not used ingame or as part of gameplay.
-	function setCruisingSpeed(speed:Float):Void;
+
+	// CLIENT ONLY
 	
 	// Update (client side values)
 	function update(dt:Float):Void;
+	
+	// cruising speed => "comfortable" moving speed. Currently not used ingame or as part of gameplay.
+	function setCruisingSpeed(speed:Float):Void;
 	
 	// Read (client side values)
 	var currentPosition(get, never):Float;
