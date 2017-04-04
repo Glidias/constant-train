e9091c612493bdc2f0866f066bf8acfe271e4830
Glidias <glidias@hotmail.com>
2017-04-04 16:05:38 +0800

    added support for @:getter shortcut for computed properties to reduce boilerplating.

--- src/cstrain/vuex/components/TrainProgressBarView.hx
+++ src/cstrain/vuex/components/TrainProgressBarView.hx
@@ -16,20 +16,23 @@ class TrainProgressBarView extends VxComponent<GameStore, NoneT, NoneT>
 
 	public static inline var NAME:String = "TrainProgressBarView";
 	
-	function get_classes_test():Dynamic {
+	@:getter function get_classes_test():Dynamic {
 		VHTMacros.return_stringMapToObj( [
 			STYLE.scrubber => true,
 		]);
 	}
 	
-	function get_scrubberStyle():Dynamic {
+	
+	@:getter function get_scrubberStyle():Dynamic {
 		var prog:Float = store.state.game.currentProgress;
 		return {
 			transform: 'translateX(${prog}%)'
 		};
 	}
-	function get_scrubStyle():Dynamic {
+
+	@:getter function get_scrubStyle():Dynamic {
 			var division:Float  = store.game.gameGetters.progressUnit;
+			
 			return {
 				width: (division*100)+"%"
 				
@@ -50,6 +53,7 @@ class TrainProgressBarView extends VxComponent<GameStore, NoneT, NoneT>
 	public function new() 
 	{
 		super();
+		
 	}
 	
 
